fbe3d4edf29ca016428d2193cf3c96baf615681f
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-01-21 14:02:17 +1100

    feat: add custom fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
 		extend: {
 			fontFamily: {
-				sans: ['Poppins']
-			}
-		}
+				sans: ['"Source Sans Pro"', ...defaultTheme.fontFamily.sans],
+				serif: ['Lora', ...defaultTheme.fontFamily.serif],
+			},
+		},
 	},
-	plugins: [require('@tailwindcss/forms')]
-};
+	plugins: [require('@tailwindcss/forms')],
+}

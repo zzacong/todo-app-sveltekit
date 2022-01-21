cf3590e06ebad73e807c5a08e705799e034460b9
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-01-21 14:02:42 +1100

    feat: add test fonts routes

---
+++ src/routes/fonts/__layout.reset.svelte
@@ -0,0 +1,34 @@
+<div class="container mx-auto pt-10">
+	<header>
+		<div class="grid grid-cols-3 mb-4">
+			<div class="flex justify-start items-center">
+				<a href="/" class="text-gray-600 hover:cursor-pointer hover:bg-gray-100 px-2 py-2 rounded"
+					><svg
+						xmlns="http://www.w3.org/2000/svg"
+						class="h-6 w-6"
+						fill="none"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+					>
+						<path
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"
+						/>
+					</svg></a
+				>
+			</div>
+			<nav class="flex justify-center space-x-10">
+				<a href="/fonts/sans" class="hover:cursor-pointer hover:bg-gray-100 px-8 py-2 rounded"
+					>Sans</a
+				>
+				<a href="/fonts/serif" class="hover:cursor-pointer hover:bg-gray-100 px-8 py-2 rounded"
+					>Serif</a
+				>
+			</nav>
+		</div>
+		<hr class="bg-gray-800 w-32 mx-auto" />
+	</header>
+	<slot />
+</div>
---
+++ src/routes/fonts/serif.svelte
@@ -0,0 +1,25 @@
+<div
+	class="text-lg md:text-xl lg:text-3xl xl:text-5xl container mx-auto text-center pt-20 grid grid-cols-2 gap-8 px-2"
+>
+	<div class="font-serif space-y-8">
+		<p class="font-extralight">Almost before we knew it.</p>
+		<p class="font-light">Almost before we knew it.</p>
+		<p class="">Almost before we knew it.</p>
+		<p class="font-medium">Almost before we knew it.</p>
+		<p class="font-semibold">Almost before we knew it.</p>
+		<p class="font-bold">Almost before we knew it.</p>
+		<p class="font-extrabold">Almost before we knew it.</p>
+		<p class="font-black">Almost before we knew it.</p>
+	</div>
+
+	<div class="font-serif space-y-8 italic">
+		<p class="font-extralight">Almost before we knew it.</p>
+		<p class="font-light">Almost before we knew it.</p>
+		<p class="">Almost before we knew it.</p>
+		<p class="font-medium">Almost before we knew it.</p>
+		<p class="font-semibold">Almost before we knew it.</p>
+		<p class="font-bold">Almost before we knew it.</p>
+		<p class="font-extrabold">Almost before we knew it.</p>
+		<p class="font-black">Almost before we knew it.</p>
+	</div>
+</div>
